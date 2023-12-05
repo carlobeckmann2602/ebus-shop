66f885358dc692df9b970347c495253acdfe9acf
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-12-05 17:17:24 +0100

    feat(shop): work in progress

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,9 +1,26 @@
 import Layout from "./_layout.tsx";
+import Card from "../components/Card.tsx";
+import { ALL_PRODUCTS } from "../commons.ts";
 
 export default function Shop() {
   return (
     <Layout>
       <h1>Shop</h1>
+      <div className="flex flex-col gap-8">
+        {ALL_PRODUCTS.map((product) => {
+          return (
+            <Card
+              key={"card" + product.id}
+              id={product.id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              imageUrl={product.imageUrl}
+              imageAlt={product.imageAlt}
+            />
+          );
+        })}
+      </div>
     </Layout>
   );
 }
