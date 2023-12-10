b038e8efbdb66eb2191e1f40030e7263bae04ff1
Carlo Beckmann <business@carlo-beckmann.com>
2023-12-10 17:53:24 +0100

    refactor: remove margin at bottom of checkout info box

--- src/components/Guide.tsx
+++ src/components/Guide.tsx
@@ -9,7 +9,7 @@ export default function Guide({ content, explanation }: GuideProps) {
   return (
     <div className="flex flex-row max-md:flex-col gap-2 w-full">
       <div className="w-1/2 max-md:w-auto p-6">{content}</div>
-      <div className="w-1/2 max-md:w-auto sticky py-4 top-0 h-screen">
+      <div className="w-1/2 max-md:w-auto sticky py-4 top-0 h-screen -m-4">
         <div className="bg-accent rounded-md px-8 py-6 shadow-lg h-full">
           {explanation}
         </div>
