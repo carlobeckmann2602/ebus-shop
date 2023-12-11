35edaf8265b37f9ccf3eddbbb75b91564831aa7b
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-12-11 09:25:15 +0100

    fix(IntroText): add basic instructions

--- src/components/checkout/infotext/IntroText.tsx
+++ src/components/checkout/infotext/IntroText.tsx
@@ -6,31 +6,30 @@ export default function IntroText() {
       title="So funktioniert's"
       content={
         <div>
-          Heading 1 Lorem ipsum dolor sit amet consectetur. Ut massa pretium nec
-          at tempor massa. Faucibus nulla eget libero ut lorem fusce. Aliquam
-          habitant dictumst non semper lectus at dignissim amet habitasse. Diam
-          ultrices sapien rhoncus nisl massa interdum in a. Amet dolor cursus
-          dolor quam diam. Molestie quis ac non iaculis. Augue euismod arcu
-          sapien volutpat lobortis. Vulputate tincidunt pharetra sagittis nunc.
-          Ut quis odio nisi tellus sit sed sit adipiscing. Eros amet est urna
-          lobortis. Odio a id sed proin euismod adipiscing. Est velit
-          condimentum consequat integer et vitae erat id neque. Odio facilisis
-          iaculis arcu magna nisi magnis. Aliquet sit ultrices viverra vitae.
-          Quis suscipit nullam vitae facilisis at cras. Adipiscing curabitur
-          eget viverra faucibus a enim. Neque malesuada purus ante ipsum euismod
-          natoque. Pellentesque velit blandit lectus diam phasellus. Eget
-          eleifend mauris pulvinar sapien quam eget. Amet elit accumsan quis
-          condimentum placerat venenatis. Massa sed duis egestas id duis nibh.
-          Facilisis nunc elit eget cursus elementum. Libero magna amet nulla
-          libero viverra sed eget aliquam nisl. Placerat dignissim vivamus
-          imperdiet turpis et urna id ullamcorper. Commodo nisi faucibus orci
-          convallis viverra nisl imperdiet netus dolor. Proin pretium id
-          consequat nisl lectus sed. Amet nunc sit viverra libero a sit. A metus
-          aliquam enim morbi sed et molestie pellentesque. Aenean pulvinar ac
-          urna amet malesuada convallis. Purus vel velit vulputate commodo
-          euismod. Sollicitudin eget orci morbi gravida. Dignissim sociis elit
-          purus eleifend nibh. Id tristique sagittis pharetra lacus duis ornare
-          et.
+          <ol className="list-decimal">
+            <li className="mb-4">
+              <b>Kundendaten</b>
+              <br />
+              Geben Sie (imaginäre) Kundendaten in den dafür vorgesehenen
+              Feldern ein.
+              <br />
+              <i>
+                (Keine Sorge: Es werden keinerlei Daten gespeichert oder
+                verarbeitet)
+              </i>
+            </li>
+            <li className="mb-4">
+              <b>Bezahlweise</b>
+              <br />
+              Wählen Sie eine der vorgeschlagenen Bezahlfunktionen.
+            </li>
+            <li className="mb-4">
+              <b>Informationen</b>
+              <br />
+              Sie erhalten an dieser Stelle hier Informationen und Wissenswertes
+              oder gegebenenfalls ein interaktives Beispiel.
+            </li>
+          </ol>
         </div>
       }
     />
