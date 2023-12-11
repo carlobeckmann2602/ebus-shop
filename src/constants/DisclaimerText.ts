c0830895042fbd18fad27f64b2368b962240daa4
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-12-11 09:20:24 +0100

    refactor: make 'du' to 'sie'

--- src/constants/DisclaimerText.ts
+++ src/constants/DisclaimerText.ts
@@ -1,6 +1,6 @@
 export const disclaimerText_1 =
-  "Willkommen bei unserem Demoshop! Hier kannst du verschiedene Produkte auswählen und in deinen Warenkorb legen, um den Bestellprozess zu testen. Keine Sorge, du musst nichts bezahlen und es werden keine Daten gespeichert. Es handelt sich nur um eine Simulation, um dir zu zeigen, wie Bezahlfunktionen in Onlineshops funktionieren.";
+  "Willkommen in unserem Demoshop! Hier können Sie verschiedene Produkte auswählen und in Ihrem Warenkorb legen, um den Bestellprozess zu testen. Keine Sorge, Sie müssen nichts bezahlen und es werden keine Daten gespeichert. Es handelt sich nur um eine Simulation, um Ihnen zu zeigen, wie Bezahlfunktionen in Onlineshops funktionieren.";
 export const disclaimerText_2 =
-  "Um loszulegen, wähle einfach ein paar Produkte aus, die dich interessieren. Du kannst die Anzahl der Produkte ändern oder sie aus dem Warenkorb entfernen, wenn du deine Meinung änderst. Wenn du fertig bist, klicke auf “Weiter zum Bezahlvorgang”, um den Bezahlvorgang zu starten.";
+  "Um loszulegen, wählen Sie einfach ein paar Produkte aus, die Sie interessieren. Sie können die Anzahl der Produkte ändern oder sie aus dem Warenkorb entfernen, wenn Sie Ihre Meinung ändern. Wenn Sie fertig sind, klicken Sie auf “Weiter zum Bezahlvorgang”, um den Bezahlvorgang zu starten.";
 export const disclaimerText_3 =
-  "Beim Bezahlvorgang kannst du zwischen verschiedenen Bezahlmethoden wählen, um dir diese genauer erklären zu lassen.";
+  "Beim Bezahlvorgang können Sie zwischen verschiedenen Bezahlmethoden wählen, um Ihnen diese genauer erklären zu lassen.";
